5d8730777876caeb78a059130802603f70a96563
Paulo Mendes <paulomendes@Paulos-Mac-mini.local>
2013-08-27 21:11:09 -0300

    Initial Commit

---
+++ Salmo do Dia/SDDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SDDAppDelegate.h
+//  Salmo do Dia
+//
+//  Created by Paulo Mendes on 8/27/13.
+//  Copyright (c) 2013 Paulo Mendes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Salmo do Dia/SDDViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDDViewController.h
+//  Salmo do Dia
+//
+//  Created by Paulo Mendes on 8/27/13.
+//  Copyright (c) 2013 Paulo Mendes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDDViewController : UIViewController
+
+@end
