8080c1edae1dcf0c66fb3d7e4f7965d2f8e6ef54
Paulo Mendes <paulo.mendes87@gmail.com>
2013-08-27 21:40:05 -0300

    setting up databaes

---
+++ Salmo do Dia/SDDHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDDHomeViewController.h
+//  Salmo do Dia
+//
+//  Created by Paulo Mendes on 8/27/13.
+//  Copyright (c) 2013 Paulo Mendes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDDHomeViewController : UIViewController
+
+@end
---
+++ Salmo do Dia/SDDHomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  SDDHomeViewController.m
+//  Salmo do Dia
+//
+//  Created by Paulo Mendes on 8/27/13.
+//  Copyright (c) 2013 Paulo Mendes. All rights reserved.
+//
+
+#import "SDDHomeViewController.h"
+
+@interface SDDHomeViewController ()
+
+@end
+
+@implementation SDDHomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
